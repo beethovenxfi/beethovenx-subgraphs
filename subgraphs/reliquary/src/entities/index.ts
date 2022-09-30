f918fded7f24c7effe1d190c098f6e4abd183ee5
0xSkly <skly.beets@protonmail.com>
2022-09-30 18:02:11 +0200

    Add reliquary subgraph

---
+++ subgraphs/reliquary/src/entities/index.ts
@@ -0,0 +1,209 @@
+import {
+  Address,
+  BigDecimal,
+  BigInt,
+  Bytes,
+  dataSource,
+} from "@graphprotocol/graph-ts";
+import {
+  DailyPoolSnapshot,
+  EmissionCurve,
+  Harvest,
+  Pool,
+  Relic,
+  Reliquary,
+  Rewarder,
+  Token,
+  User,
+} from "../../generated/schema";
+import { Reliquary as ReliquaryContract } from "../../generated/Reliquary/Reliquary";
+import { Rewarder as RewarderContract } from "../../generated/Reliquary/Rewarder";
+import { ERC20 } from "../../generated/Reliquary/ERC20";
+import { scaleDown } from "../utils/numbers";
+
+const DAY = 24 * 60 * 60;
+
+export function getOrCreateReliquary(): Reliquary {
+  const address = dataSource.address();
+  let reliquary = Reliquary.load(address);
+
+  if (reliquary === null) {
+    const reliquaryContract = ReliquaryContract.bind(address);
+    const emissionCurve = getOrCreateEmissionCurve(
+      reliquaryContract.emissionCurve()
+    );
+    reliquary = new Reliquary(address);
+    reliquary.emissionToken = reliquaryContract.oath();
+    reliquary.emissionCurve = emissionCurve.id;
+    reliquary.totalAllocPoint = reliquaryContract.totalAllocPoint().toI32();
+    reliquary.poolCount = reliquaryContract.poolLength().toI32();
+    reliquary.relicCount = reliquaryContract.totalSupply().toI32();
+    reliquary.save();
+  }
+  return reliquary;
+}
+
+export function getOrCreateEmissionCurve(address: Address): EmissionCurve {
+  let emissionCurve = EmissionCurve.load(address);
+
+  if (emissionCurve === null) {
+    emissionCurve = new EmissionCurve(address);
+    emissionCurve.address = address;
+    emissionCurve.save();
+  }
+
+  return emissionCurve;
+}
+
+export function createPool(
+  pid: i32,
+  allocPoint: i32,
+  poolTokenAddress: Address,
+  rewarderAddress: Address,
+  nftDescriptor: Address
+): Pool {
+  const contract = ReliquaryContract.bind(dataSource.address());
+  const poolInfo = contract.getPoolInfo(BigInt.fromI32(pid));
+  const levelInfo = contract.getLevelInfo(BigInt.fromI32(pid));
+
+  const poolToken = getOrCreateToken(poolTokenAddress);
+
+  const pool = new Pool(Bytes.fromI32(pid));
+  pool.pid = pid;
+  pool.name = poolInfo.name;
+  pool.reliquary = dataSource.address();
+  pool.nftDescriptor = nftDescriptor;
+  pool.poolToken = poolToken.id;
+  pool.poolTokenAddress = poolTokenAddress;
+  pool.totalBalance = BigDecimal.zero();
+  pool.relicCount = 0;
+  pool.levelMaturities = levelInfo.requiredMaturity;
+  pool.allocPoint = allocPoint;
+  if (rewarderAddress !== Address.zero()) {
+    const rewarder = getOrCreateRewarder(pid, rewarderAddress);
+    pool.rewarder = rewarder.id;
+  }
+  pool.save();
+  return pool;
+}
+
+export function getPoolOrThrow(pid: i32): Pool {
+  const pool = Pool.load(Bytes.fromI32(pid));
+  if (pool === null) {
+    throw new Error("Pool does not exist - " + pid.toString());
+  }
+  return pool;
+}
+
+export function getOrCreateRewarder(
+  pid: i32,
+  rewarderAddress: Address
+): Rewarder {
+  const rewarderId = Bytes.fromI32(pid).concat(rewarderAddress);
+  let rewarder = Rewarder.load(rewarderAddress);
+
+  if (rewarder === null) {
+    const contract = RewarderContract.bind(rewarderAddress);
+    const rewardToken = getOrCreateToken(contract.rewardToken());
+    rewarder = new Rewarder(rewarderId);
+    rewarder.rewardToken = rewardToken.id;
+    rewarder.save();
+  }
+  return rewarder;
+}
+
+export function getOrCreateDailyPoolSnapshot(
+  pid: i32,
+  timestamp: i32
+): DailyPoolSnapshot {
+  let timestampStartOfDay = timestamp - (timestamp % DAY);
+  const snpashotId = Bytes.fromI32(pid).concatI32(timestampStartOfDay);
+  let snapshot = DailyPoolSnapshot.load(snpashotId);
+
+  if (snapshot === null) {
+    snapshot = new DailyPoolSnapshot(snpashotId);
+    snapshot.timestamp = timestampStartOfDay;
+    snapshot.totalBalance = BigDecimal.zero();
+    snapshot.totalDeposited = BigDecimal.zero();
+    snapshot.totalWithdrawn = BigDecimal.zero();
+    snapshot.relicCount = 0;
+    snapshot.save();
+  }
+  return snapshot;
+}
+
+export function createRelic(userAddress: Address, relicId: i32): Relic {
+  const reliquaryContract = ReliquaryContract.bind(dataSource.address());
+  const positionInfo = reliquaryContract.getPositionForId(
+    BigInt.fromI32(relicId)
+  );
+  const pool = getPoolOrThrow(positionInfo.poolId.toI32());
+  const user = getOrCreateUser(userAddress);
+
+  const relic = new Relic(Bytes.fromI32(relicId));
+  relic.relicId = relicId;
+  relic.reliquary = dataSource.address();
+  relic.pool = pool.id;
+  relic.pid = pool.pid;
+  relic.user = user.id;
+  relic.balance = BigDecimal.zero();
+  relic.entryTimestamp = positionInfo.entry.toI32();
+  relic.save();
+
+  return relic;
+}
+
+export function getRelicOrThrow(relicId: i32): Relic {
+  const relic = Relic.load(Bytes.fromI32(relicId));
+  if (relic === null) {
+    throw new Error("Relic not found - " + relicId.toString());
+  }
+  return relic;
+}
+
+export function getOrCreateUser(address: Address): User {
+  let user = User.load(address);
+
+  if (user === null) {
+    user = new User(address);
+    user.reliquary = dataSource.address();
+    user.save();
+  }
+  return user;
+}
+
+export function getOrCreateToken(address: Address): Token {
+  let token = Token.load(address);
+  if (token === null) {
+    const erc20 = ERC20.bind(address);
+    token = new Token(address);
+    token.address = address;
+    token.name = erc20.name();
+    token.symbol = erc20.symbol();
+    token.decimals = erc20.decimals();
+    token.save();
+  }
+  return token;
+}
+
+export function createHarvest(
+  relicId: i32,
+  amount: BigInt,
+  timestamp: i32
+): Harvest {
+  const reliquary = getOrCreateReliquary();
+  const token = getOrCreateToken(Address.fromBytes(reliquary.emissionToken));
+  const relic = getRelicOrThrow(relicId);
+  const reliquaryContract = ReliquaryContract.bind(dataSource.address());
+  const owner = reliquaryContract.ownerOf(BigInt.fromI32(relicId));
+  const harvest = new Harvest(Bytes.fromI32(relicId).concatI32(timestamp));
+
+  harvest.amount = scaleDown(amount, token.decimals);
+  harvest.token = token.id;
+  harvest.timestamp = timestamp;
+  harvest.reliquary = reliquary.id;
+  harvest.relic = relic.id;
+  harvest.user = owner;
+  harvest.save();
+  return harvest;
+}
---
+++ subgraphs/reliquary/src/mappings/reliquary.ts
@@ -0,0 +1,236 @@
+import {
+  Address,
+  BigDecimal,
+  BigInt,
+  dataSource,
+} from "@graphprotocol/graph-ts";
+import { Reliquary as ReliquaryContract } from "../../generated/Reliquary/Reliquary";
+import { Transfer } from "../../generated/Reliquary/ERC721";
+import {
+  Deposit,
+  EmergencyWithdraw,
+  Harvest,
+  LogPoolAddition,
+  LogPoolModified,
+  LogSetEmissionCurve,
+  Merge,
+  Shift,
+  Split,
+  Withdraw,
+} from "../../generated/Reliquary/Reliquary";
+import {
+  createHarvest,
+  createPool,
+  createRelic,
+  getOrCreateDailyPoolSnapshot,
+  getOrCreateEmissionCurve,
+  getOrCreateReliquary,
+  getOrCreateRewarder,
+  getOrCreateUser,
+  getPoolOrThrow,
+  getRelicOrThrow,
+} from "../entities";
+import { scaleDown } from "../utils/numbers";
+
+export function logPoolAddition(event: LogPoolAddition): void {
+  const params = event.params;
+  createPool(
+    params.pid.toI32(),
+    params.allocPoint.toI32(),
+    params.poolToken,
+    params.rewarder,
+    params.nftDescriptor
+  );
+}
+
+export function logPoolModified(event: LogPoolModified): void {
+  const params = event.params;
+  const pool = getPoolOrThrow(params.pid.toI32());
+  if (params.rewarder === Address.zero()) {
+    pool.rewarder = null;
+  } else {
+    const rewarder = getOrCreateRewarder(
+      params.pid.toI32(),
+      Address.fromBytes(params.rewarder)
+    );
+    pool.rewarder = rewarder.id;
+  }
+  pool.allocPoint = params.allocPoint.toI32();
+  pool.nftDescriptor = params.nftDescriptor;
+
+  pool.save();
+}
+
+export function setEmissionCurve(event: LogSetEmissionCurve): void {
+  const reliquary = getOrCreateReliquary();
+  const emissionCurve = getOrCreateEmissionCurve(
+    event.params.emissionCurveAddress
+  );
+  reliquary.emissionCurve = emissionCurve.id;
+  reliquary.save();
+}
+
+export function deposit(event: Deposit): void {
+  const params = event.params;
+
+  const pool = getPoolOrThrow(params.pid.toI32());
+  const relic = getRelicOrThrow(params.relicId.toI32());
+  const snapshot = getOrCreateDailyPoolSnapshot(
+    relic.pid,
+    event.block.timestamp.toI32()
+  );
+  const scaledAmount = scaleDown(params.amount, 18);
+
+  pool.totalBalance = pool.totalBalance.plus(scaledAmount);
+  pool.save();
+  const reliquaryContract = ReliquaryContract.bind(dataSource.address());
+  const positionInfo = reliquaryContract.getPositionForId(params.relicId);
+  relic.entryTimestamp = positionInfo.entry.toI32();
+  relic.balance = relic.balance.plus(scaledAmount);
+  relic.save();
+
+  snapshot.totalDeposited = snapshot.totalDeposited.plus(scaledAmount);
+  snapshot.save();
+}
+
+export function withdraw(event: Withdraw): void {
+  const params = event.params;
+
+  const pool = getPoolOrThrow(params.pid.toI32());
+  const relic = getRelicOrThrow(params.relicId.toI32());
+  const snapshot = getOrCreateDailyPoolSnapshot(
+    relic.pid,
+    event.block.timestamp.toI32()
+  );
+  const scaledAmount = scaleDown(params.amount, 18);
+
+  pool.totalBalance = pool.totalBalance.minus(scaledAmount);
+  pool.save();
+  relic.balance = relic.balance.minus(scaledAmount);
+  relic.save();
+
+  snapshot.totalDeposited = snapshot.totalDeposited.minus(scaledAmount);
+  snapshot.save();
+}
+
+export function emergencyWithdraw(event: EmergencyWithdraw): void {
+  const params = event.params;
+
+  const pool = getPoolOrThrow(params.pid.toI32());
+  const relic = getRelicOrThrow(params.relicId.toI32());
+  const snapshot = getOrCreateDailyPoolSnapshot(
+    relic.pid,
+    event.block.timestamp.toI32()
+  );
+  const scaledAmount = scaleDown(params.amount, 18);
+  pool.totalBalance = pool.totalBalance.minus(scaledAmount);
+  pool.save();
+  relic.balance = BigDecimal.zero();
+  relic.save();
+
+  snapshot.totalDeposited = snapshot.totalDeposited.minus(scaledAmount);
+  snapshot.save();
+}
+
+export function harvest(event: Harvest): void {
+  const params = event.params;
+  if (params.amount.gt(BigInt.zero())) {
+    createHarvest(
+      params.relicId.toI32(),
+      params.amount,
+      event.block.timestamp.toI32()
+    );
+  }
+}
+
+export function split(event: Split): void {
+  const params = event.params;
+  const relicFrom = getRelicOrThrow(params.fromId.toI32());
+  const relicTo = getRelicOrThrow(params.toId.toI32());
+
+  const scaledAmount = scaleDown(params.amount, 18);
+
+  relicFrom.balance = relicFrom.balance.minus(scaledAmount);
+  relicFrom.save();
+  relicTo.balance = scaledAmount;
+  relicTo.entryTimestamp = relicFrom.entryTimestamp;
+  relicTo.save();
+}
+
+export function shift(event: Shift): void {
+  const params = event.params;
+
+  const relicFrom = getRelicOrThrow(params.fromId.toI32());
+  const relicTo = getRelicOrThrow(params.toId.toI32());
+
+  const scaledAmount = scaleDown(params.amount, 18);
+
+  const reliquaryContract = ReliquaryContract.bind(dataSource.address());
+  const positionInfoRelicTo = reliquaryContract.getPositionForId(params.toId);
+
+  relicFrom.balance = relicFrom.balance.minus(scaledAmount);
+  relicFrom.save();
+  relicTo.balance = relicTo.balance.plus(scaledAmount);
+  relicTo.entryTimestamp = positionInfoRelicTo.entry.toI32();
+  relicTo.save();
+}
+
+export function merge(event: Merge): void {
+  const params = event.params;
+
+  const relicFrom = getRelicOrThrow(params.fromId.toI32());
+  const relicTo = getRelicOrThrow(params.toId.toI32());
+
+  const scaledAmount = scaleDown(params.amount, 18);
+
+  const reliquaryContract = ReliquaryContract.bind(dataSource.address());
+  const positionInfoRelicTo = reliquaryContract.getPositionForId(params.toId);
+
+  relicFrom.balance = BigDecimal.zero();
+  relicFrom.save();
+  relicTo.balance = relicTo.balance.plus(scaledAmount);
+  relicTo.entryTimestamp = positionInfoRelicTo.entry.toI32();
+  relicTo.save();
+}
+
+export function transfer(event: Transfer): void {
+  if (event.params.from === Address.zero()) {
+    // mint
+    const relic = createRelic(event.params.to, event.params.tokenId.toI32());
+    const reliquary = getOrCreateReliquary();
+    reliquary.relicCount++;
+    reliquary.save();
+
+    const pool = getPoolOrThrow(relic.pid);
+    pool.relicCount++;
+    pool.save();
+    const snapshot = getOrCreateDailyPoolSnapshot(
+      relic.pid,
+      event.block.timestamp.toI32()
+    );
+    snapshot.relicCount = pool.relicCount;
+    snapshot.save();
+  } else if (event.params.to === Address.zero()) {
+    // burn
+    const relic = getRelicOrThrow(event.params.tokenId.toI32());
+    const reliquary = getOrCreateReliquary();
+    reliquary.relicCount--;
+    reliquary.save();
+
+    const pool = getPoolOrThrow(relic.pid);
+    pool.relicCount--;
+    pool.save();
+    const snapshot = getOrCreateDailyPoolSnapshot(
+      relic.pid,
+      event.block.timestamp.toI32()
+    );
+    snapshot.relicCount = pool.relicCount;
+    snapshot.save();
+  } else {
+    // account transfer
+    const relic = getRelicOrThrow(event.params.tokenId.toI32());
+    const userTo = getOrCreateUser(Address.fromBytes(relic.user));
+    relic.user = userTo.id;
+    relic.save();
+  }
+}
